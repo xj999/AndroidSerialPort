a150e8ad3ec5e55e62e0bae0ec230f0c15abfd24
luxj <358231333@qq.com>
2024-12-16 11:21:19 +0800

    fix bug

--- app/src/main/java/com/vi/androidserialport/MainActivity.kt
+++ app/src/main/java/com/vi/androidserialport/MainActivity.kt
@@ -1,26 +1,28 @@
 package com.vi.androidserialport
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.vi.androidserialport.databinding.ActivityMainBinding
 import com.vi.vioserial.NormalSerial
 import com.vi.vioserial.listener.OnNormalDataListener
-import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * 快速使用
  * Quick use
  * @author Vi
  * @date 2019-07-17 16:50
- * @e-mail 380180278@qq.com
+ * @e-mail cfop_f2l@163.com
  */
 class MainActivity : AppCompatActivity(), OnNormalDataListener {
 
     var isOpenSerial = false //串口是否打开 Is the serial port open?
-
+    private lateinit var binding: ActivityMainBinding // ViewBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         initClick()
 
@@ -28,16 +30,16 @@ class MainActivity : AppCompatActivity(), OnNormalDataListener {
 
     override fun onDestroy() {
         super.onDestroy()
-        //【移除数据监听 remove listener】
         NormalSerial.instance().removeDataListener(this)
         NormalSerial.instance().close()
     }
 
+    @SuppressLint("StringFormatMatches")
     private fun initClick() {
         //连接/断开串口 Connect/disconnect the serial port
-        mBtnConnect.setOnClickListener {
-            val ck = mEtCK.text.toString()
-            val btl = mEtBTL.text.toString()
+        binding.mBtnConnect.setOnClickListener {
+            val ck = binding.mEtCK.text.toString()
+            val btl = binding.mEtBTL.text.toString()
             if (ck.isBlank() || btl.isBlank()) {
                 Toast.makeText(this@MainActivity, resources.getString(R.string.text_full_data), Toast.LENGTH_SHORT)
                     .show()
@@ -45,11 +47,11 @@ class MainActivity : AppCompatActivity(), OnNormalDataListener {
             }
 
             if (!isOpenSerial) {
-                // 【第1步：打开串口 Open serial port 】
+                //打开串口 Open serial port
                 val openStatus = NormalSerial.instance().open(ck, btl.toInt())
                 if (openStatus == 0) {
                     isOpenSerial = true
-                    mBtnConnect.text = resources.getString(R.string.text_disconnect)
+                    binding.mBtnConnect.text = resources.getString(R.string.text_disconnect)
                     Toast.makeText(
                         this@MainActivity,
                         resources.getString(R.string.text_open_success),
@@ -57,7 +59,7 @@ class MainActivity : AppCompatActivity(), OnNormalDataListener {
                     ).show()
                 } else {
                     isOpenSerial = false
-                    mBtnConnect.text = resources.getString(R.string.text_connect)
+                    binding.mBtnConnect.text = resources.getString(R.string.text_connect)
                     Toast.makeText(
                         this@MainActivity,
                         String.format(resources.getString(R.string.text_open_fail), openStatus),
@@ -65,18 +67,18 @@ class MainActivity : AppCompatActivity(), OnNormalDataListener {
                     ).show()
                 }
 
-                //【添加数据接收回调 Add data receive callback】
+                //添加数据接收回调 Add data receive callback
                 NormalSerial.instance().addDataListener(this)
             } else {
                 isOpenSerial = false
-                mBtnConnect.text = resources.getString(R.string.text_connect)
+                binding.mBtnConnect.text = resources.getString(R.string.text_connect)
                 NormalSerial.instance().close()
             }
         }
 
-        //【往串口发送数据 Send data to the serial port】
-        mBtnSend.setOnClickListener {
-            val input = mEtInput.text.toString()
+        //往串口发送数据 Send data to the serial port
+        binding.mBtnSend.setOnClickListener {
+            val input = binding.mEtInput.text.toString()
             if (input.isBlank()) {
                 Toast.makeText(this@MainActivity, resources.getString(R.string.text_full_send_Data), Toast.LENGTH_SHORT)
                     .show()
@@ -93,17 +95,22 @@ class MainActivity : AppCompatActivity(), OnNormalDataListener {
             }
 
             NormalSerial.instance().sendHex(input)
-            mTvSendData.append("\n$input")
+            binding.mTvSendData.append("\n$input")
         }
 
-        mTvSendData.setOnClickListener { mTvSendData.text = "" }
-        mTvReviData.setOnClickListener { mTvReviData.text = "" }
+//        mTvOther.setOnClickListener {
+//            startActivity(Intent(this@MainActivity, VioActivity::class.java))
+//        }
+
+        binding.mTvSendData.setOnClickListener { binding.mTvSendData.text = "" }
+        binding.mTvReviData.setOnClickListener { binding.mTvReviData.text = "" }
     }
 
     /**
      * 串口返回数据 Serial port return data
      */
     override fun normalDataBack(data: String?) {
+        binding.
         mTvReviData.append("\n$data")
     }
 
